3ae91664387b80fa1773c368bc89dec63a9ee876
Colombine972 <decidela972@me.com>
2025-12-17 13:29:39 +0100

    finish defined  all path

--- src/data/flightRoutes.ts
+++ src/data/flightRoutes.ts
@@ -8,34 +8,6 @@ export type FlightRoute = {
 };
 
 export const flightRoutes: FlightRoute[] = [
-	//	{
-	//		id: "route-outside-about",
-	//		d: "M-40 150 Q90 80 230 150",
-	//		duration: 4,
-	//	},
-	//	{
-	//		id: "route-about-skills",
-	//		d: "M230 150 Q330 50 430 120",
-	//		duration: 4,
-	//	},
-	//
-	//	{
-	//		id: "route-skills-projects",
-	//		d: "M430 120 Q540 40 640 100",
-	//		duration: 4,
-	//	},
-	//	{
-	//		id: "route-projects-contact",
-	//		d: "M640 100 Q560 260 480 275",
-	//		duration: 4,
-	//	},
-	//	{
-	//		id: "route-contact-cv",
-	//		d: "M480 275 Q620 360 720 340",
-	//		duration: 4,
-	//	},
-	//];
-
 	//	ABOUT
 	{
 		from: "outside",
@@ -46,7 +18,7 @@ export const flightRoutes: FlightRoute[] = [
 	{
 		from: "about",
 		to: "skills",
-		d: "M230 150 Q330 50 430 120",
+		d: "M230 150 L430 120",
 		duration: 4,
 	},
 	{
@@ -58,13 +30,13 @@ export const flightRoutes: FlightRoute[] = [
 	{
 		from: "about",
 		to: "contact",
-		d: "M230 150 Q330 50 480 275",
+		d: "M260 160 L480 275",
 		duration: 4,
 	},
 	{
 		from: "about",
 		to: "cv",
-		d: "M230 150 Q330 50 720 340",
+		d: "M250 150 L720 340",
 		duration: 4,
 	},
 
@@ -78,25 +50,25 @@ export const flightRoutes: FlightRoute[] = [
 	{
 		from: "skills",
 		to: "about",
-		d: "M430 120 Q330 50 230 150",
+		d: "M450 130 L230 150",
 		duration: 4,
 	},
 	{
 		from: "skills",
 		to: "contact",
-		d: "M430 120 Q330 50 480 275",
+		d: "M430 150  L480 275",
 		duration: 4,
 	},
 	{
 		from: "skills",
 		to: "projects",
-		d: "M430 120 Q330 50 640 100",
+		d: "M430 120 L640 100",
 		duration: 4,
 	},
 	{
 		from: "skills",
 		to: "cv",
-		d: "M430 120 Q330 50 720 340",
+		d: "M430 150 L720 340",
 		duration: 4,
 	},
 
@@ -122,75 +94,75 @@ export const flightRoutes: FlightRoute[] = [
 	{
 		from: "projects",
 		to: "contact",
-		d: "M640 100 Q330 50 480 275",
+		d: "M640 100 L480 275",
 		duration: 4,
 	},
 	{
 		from: "projects",
 		to: "cv",
-		d: "M640 100 Q330 50 720 340",
+		d: "M640 110 L720 340",
 		duration: 4,
 	},
 	//	CONTACT
 	{
 		from: "outside",
 		to: "contact",
-		d: "M-40 150 Q90 80 480 275",
+		d: "M-40 150 Q250 40 480 275",
 		duration: 4,
 	},
 	{
 		from: "contact",
 		to: "about",
-		d: "M480 275 Q330 50 230 150",
+		d: "M480 275 L230 150",
 		duration: 4,
 	},
 	{
 		from: "contact",
 		to: "skills",
-		d: "M480 275 Q330 50 430 150",
+		d: "M480 275 L430 150",
 		duration: 4,
 	},
 	{
 		from: "contact",
 		to: "projects",
-		d: "M480 275 Q330 50 640 100",
+		d: "M480 275 L640 100",
 		duration: 4,
 	},
 	{
 		from: "contact",
 		to: "cv",
-		d: "M480 275 Q330 50 720 340",
+		d: "M500 275 L720 340",
 		duration: 4,
 	},
 	//	CV
 	{
 		from: "outside",
 		to: "cv",
-		d: "M-40 150 Q90 80 720 340",
+		d: "M-40 150 Q90 120 720 340",
 		duration: 4,
 	},
 	{
 		from: "cv",
 		to: "about",
-		d: "M720 340 Q330 50 230 150",
+		d: "M720 340 L250 150",
 		duration: 4,
 	},
 	{
 		from: "cv",
 		to: "skills",
-		d: "M720 340 Q330 50 430 150",
+		d: "M720 340 L 430 150",
 		duration: 4,
 	},
 	{
 		from: "cv",
 		to: "projects",
-		d: "M720 340 Q330 50 640 100",
+		d: "M720 340 L640 120",
 		duration: 4,
 	},
 	{
 		from: "cv",
 		to: "contact",
-		d: "M720 340 Q330 50 480 275",
+		d: "M720 340 L500 275",
 		duration: 4,
 	},
 ];
